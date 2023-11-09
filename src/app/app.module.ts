0b49c40fd2a9cd179f6633a1e8c0555c9bc1f7f7
L30Gt <leeo12322@hotmail.com>
2023-11-09 18:20:51 -0300

    feat: Implemented Two way binding and event binding in 'cadastrar-produto' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { CadastrarProdutoComponent } from './components/produtos/cadastrar-produ
 import { LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 registerLocaleData(localePt);
 
@@ -26,7 +28,8 @@ registerLocaleData(localePt);
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
--- src/app/components/produtos/cadastrar-produto/cadastrar-produto.component.ts
+++ src/app/components/produtos/cadastrar-produto/cadastrar-produto.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cadastrar-produto',
   templateUrl: './cadastrar-produto.component.html',
   styleUrls: ['./cadastrar-produto.component.css']
 })
-export class CadastrarProdutoComponent {
+export class CadastrarProdutoComponent implements OnInit {
 
+  nome: string = '';
+  validade: string = '';
+  preco: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  salvarProduto(): void {
+    console.log('Nome: ', this.nome);
+    console.log('Nome: ', this.validade);
+    console.log('Nome: ', this.preco);
+    alert("Salvo com sucesso!");
+  }
 }
