b33d109b39447b6b54b2fbe159b66db567c9ccff
L30Gt <leeo12322@hotmail.com>
2023-11-16 15:05:13 -0300

    feat: Added service and interface 'produto'. Implemented CRUD method GET for product listing

---
+++ src/app/services/produtos.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduto } from '../model/IProduto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+  private URL: string = 'http://localhost:3000/produtos';
+
+  constructor(private http: HttpClient) { }
+
+  buscarTodos(): Observable<IProduto[]>{
+    return this.http.get<IProduto[]>(this.URL);
+  }
+}
