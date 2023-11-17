b079c28b14fd7835446f63f9a24afd51c9a6b7f4
L30Gt <leeo12322@hotmail.com>
2023-11-17 11:54:52 -0300

    feat: Added error message using ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { registerLocaleData } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 
 registerLocaleData(localePt);
@@ -33,7 +35,9 @@ registerLocaleData(localePt);
     AppRoutingModule,
     FormsModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' },
--- src/app/services/produtos.service.ts
+++ src/app/services/produtos.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { IProduto } from '../model/IProduto.model';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,21 @@ import { IProduto } from '../model/IProduto.model';
 export class ProdutosService {
   private URL: string = 'http://localhost:3000/produtos';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   buscarTodos(): Observable<IProduto[]>{
-    return this.http.get<IProduto[]>(this.URL);
+    return this.http.get<IProduto[]>(this.URL).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+      );
+  }
+
+  exibirErro(e: any): Observable<any> {
+    this.exibirMensagem('Erro!!!', 'Não foi possível realizar a operação', 'toast-error');
+    return EMPTY;
+  }
+
+  exibirMensagem(titulo: string, mensagem:string, tipo:string):void {
+    this.toastr.show(mensagem, titulo, {closeButton:true, progressBar:true}, tipo);
   }
 }
